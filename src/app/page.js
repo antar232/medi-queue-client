cd1482058758c486dbb90154e965ff57d9fe9f2f
antar232 <dasantar451@gmail.com>
2026-05-18 01:47:23 +0600

    Implement a MediQueue section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import HomePage from "@/components/HomePage";
+import MediQueue from "@/components/MediQueue";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <HomePage/>
       <HeroSection/>
+      <MediQueue/>
     </div>
   );
 }
